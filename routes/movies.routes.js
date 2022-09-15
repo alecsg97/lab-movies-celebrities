c0d3e3184fafc53f24deada2358e25ab44f73bf5
Alec Santamaria <alec@Alecs-MBP.attlocal.net>
2022-09-15 19:20:48 -0400

    adding movies to app

--- routes/movies.routes.js
+++ routes/movies.routes.js
@@ -1,5 +1,35 @@
 const router = require("express").Router();
+const Movie = require('../models/Movie.model');
 
 // all your routes here
+
+router.get('/create', (req, res, next) => {
+    res.render('movies/new-movie');
+})
+
+router.post('/create', async (req, res, next) => {
+    try {
+        const { title, genre, plot, cast } = req.body;
+        await Celebrity.create({
+            title,
+            genre,
+            plot,
+            cast
+        });
+        res.redirect('/movies');
+    } catch(error) {
+            next(error);
+        res.render('movies/new-movie');
+    }
+})
+
+router.get('/', async (req, res, next) => {
+    try {
+        const movies = await Movie.find();
+        res.render('movies/movies', { movies });
+    } catch(error) {
+        next(error);
+    }
+})
 
 module.exports = router;
